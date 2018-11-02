b4c2fc2d97958fd6376cb6ed5b06801e97df8ed0
gunwoo.jung <doscmDev@gmail.com>
2018-11-02 15:07:39 +0900

    Refactor : _convertTen
    object 와 Number type 이 가능하게 설정

--- key.js
+++ key.js
@@ -2,7 +2,7 @@ function _p(key, len) {
   var setBit = [];
   setBit = (len == 10 ? _setBit(key, len) : key);
   var list = (len == 10 ? [3, 5, 2, 7, 4, 10, 1, 9, 8, 6] :
-    len == 8 ? [6, 3, 7, 4, 8, 5, 10, 9] : []);
+    len == 8 ? [6, 3, 7, 4, 8, 5, 10, 9] : [2, 4, 3, 1]);
   var result = [];
   for (var i = 0; i < len; i++) {
     result[i] = setBit[list[i] - 1];
@@ -18,8 +18,19 @@ function _getBit(key) {
   return result;
 }
 
-function _convertTen(num) {
-  result = parseInt(num, 2);
+function _convertTen(bin) {
+  var result = 0;
+  if (typeof bin === 'object') {
+    for (i = 0; i < bin.length; i++) {
+      result |= bin[i];
+      if (i != bin.length - 1) {
+        result = result << 1;
+      }
+    }
+  } else {
+    bin *= 1;
+    var result = parseInt(bin, 2);
+  }
   return result;
 }
 
@@ -38,6 +49,24 @@ function _shl(key, cnt) {
     for (var i = 0; i < cnt; i++) {
       var lk = key.slice(0, 5);
       var rk = key.slice(5, 10);
+      var lkH = lk[0];
+      var rkH = rk[0];
+      for (var j = 0; j < lk.length; j++) {
+        lk[j] = lk[j + 1];
+        rk[j] = rk[j + 1];
+        if (j == lk.length - 1) {
+          lk[j] = lkH;
+          rk[j] = rkH;
+        }
+      }
+      key = result = lk.concat(rk);
+    }
+    return result;
+  } else if (key.length == 8) {
+    var result = [];
+    for (var i = 0; i < cnt; i++) {
+      var lk = key.slice(0, 4);
+      var rk = key.slice(4, 8);
       var lkH = lk[0];
       var rkH = rk[0];
       for (var j = 0; j < lk.length; j++) {
