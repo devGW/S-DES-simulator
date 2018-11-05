38114197f9fb3a652f8b4348f34164600d7d287c
gunwoo.jung <doscmDev@gmail.com>
2018-11-05 14:13:19 +0900

    Change : function name sleep() -> _sleep()

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ function _reload() {
 }
 
 
-function sleep(t) {
+function _sleep(t) {
   return new Promise(resolve => setTimeout(resolve, t));
 }
 
@@ -21,15 +21,15 @@ function _keyRun() {
       var shift2 = _shl(shift1, 2);
       var k2 = _p(shift2, 8);
       document.getElementById("P10").value = _getBit(p10);
-      await sleep(100);
+      await _sleep(100);
       document.getElementById("Shift1").value = _getBit(shift1);
-      await sleep(100);
+      await _sleep(100);
       document.getElementById("K1").value = _getBit(k1);
-      await sleep(100);
+      await _sleep(100);
       document.getElementById("Shift2").value = _getBit(shift2);
-      await sleep(100);
+      await _sleep(100);
       document.getElementById("K2").value = _getBit(k2);
-      await sleep(100);
+      await _sleep(100);
       var plaintext1 = document.getElementById("plaintext1").value;
       var enc = "";
       var encF = "";
@@ -52,21 +52,21 @@ function _keyRun() {
             decBit |= _convertTen(ipm1);
             j == 0 ? decBit = decBit << 8 : decBit = decBit;
             document.getElementById("IP1").value = _getBit(ip); // set ip
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("fk1").value = _getBit(fk); // set fk
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("SW1").value = _getBit(sw); // fk switch
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("fk2").value = _getBit(fk2); // set second f2
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("IPm1").value = _getBit(ipm1); // set ip-1
-            await sleep(100);
+            await _sleep(100);
             enc = String.fromCharCode(decBit);
             document.getElementById("encrypt1").value == "" ? document.getElementById("encrypt1").value = enc : document.getElementById("encrypt1").value = encF + enc;
           }
           encF += enc;
           document.getElementById("encrypt2").value = encF; // set decrytion plaintext
-          await sleep(100);
+          await _sleep(100);
         } else {
           var ip = _ip(plaintext1[i].charCodeAt(0), 8, 1);
           var fk = _fk(ip, k1);
@@ -77,19 +77,19 @@ function _keyRun() {
           encF += String.fromCharCode(_convertTen(ipm1));
 
           document.getElementById("IP1").value = _getBit(ip);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("fk1").value = _getBit(fk);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("SW1").value = _getBit(sw);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("fk2").value = _getBit(fk2);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("IPm1").value = _getBit(ipm1);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("encrypt1").value = encF;
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("encrypt2").value = encF;
-          await sleep(100);
+          await _sleep(100);
         }
       }
 
@@ -109,18 +109,18 @@ function _keyRun() {
             j == 0 ? decBit = decBit << 8 : decBit = decBit;
 
             document.getElementById("IP2").value = _getBit(ip);
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("fkOne").value = _getBit(fk1);
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("SW2").value = _getBit(sw);
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("fkTwo").value = _getBit(fk2);
-            await sleep(100);
+            await _sleep(100);
             document.getElementById("IPm2").value = _getBit(ipm1);
-            await sleep(100);
+            await _sleep(100);
             dec = String.fromCharCode(decBit);
             document.getElementById("plaintext2").value == "" ? document.getElementById("plaintext2").value = dec : document.getElementById("plaintext2").value = decF + dec;
-            await sleep(100);
+            await _sleep(100);
           }
           decF += dec;
         } else {
@@ -131,17 +131,17 @@ function _keyRun() {
           var ipm1 = _ip(_convertTen(fk2), 8, 2);
           decF += String.fromCharCode(_convertTen(ipm1));
           document.getElementById("IP2").value = _getBit(ip);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("fkOne").value = _getBit(fk1);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("SW2").value = _getBit(sw);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("fkTwo").value = _getBit(fk2);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("IPm2").value = _getBit(ipm1);
-          await sleep(100);
+          await _sleep(100);
           document.getElementById("plaintext2").value = decF;
-          await sleep(100);
+          await _sleep(100);
         }
       }
     }
