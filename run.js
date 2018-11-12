b3774949e950c9e6ec22eb29b303d28629d44caa
gunwoo.jung <doscmDev@gmail.com>
2018-11-12 14:51:54 +0900

    short : code line
    line shorten

--- run.js
+++ run.js
@@ -11,7 +11,7 @@ const _sleep = t => {
 const _keyRun = async () => {
   const keyId = document.getElementById("key");
   const key = keyId ? keyId.value : undefined;
-  if (key < 0 || key > 1023 || key =="") {
+  if (key < 0 || key > 1023 || key == "") {
     alert("Key value error\nKey range from 0 to 1023.");
     return;
   } else {
@@ -21,24 +21,12 @@ const _keyRun = async () => {
     const shift2 = _shl(shift1, 2);
     const k2 = _p(shift2, 8);
     const printKey = [
-      () => {
-        document.getElementById("P10").value = _getBit(p10);
-      },
-      () => {
-        document.getElementById("Shift1").value = _getBit(shift1);
-      },
-      () => {
-        document.getElementById("K1").value = _getBit(k1);
-      },
-      () => {
-        document.getElementById("Shift2").value = _getBit(shift2);
-      },
-      () => {
-        document.getElementById("K2").value = _getBit(k2);
-      },
-      () => {
-        return document.getElementById("plaintext1").value;
-      }
+      () => document.getElementById("P10").value = _getBit(p10),
+      () => document.getElementById("Shift1").value = _getBit(shift1),
+      () => document.getElementById("K1").value = _getBit(k1),
+      () => document.getElementById("Shift2").value = _getBit(shift2),
+      () => document.getElementById("K2").value = _getBit(k2),
+      () => document.getElementById("plaintext1").value
     ];
 
     for (let show of printKey) {
@@ -55,125 +43,100 @@ const _keyRun = async () => {
     for (let i = 0; i < plaintext1.length; i++) {
       let encBit = 0, ip, fk1, sw, fk2, ipm1;
       const printEnc = [
-        () => {
-          document.getElementById("IP1").value = _getBit(ip);
-        },
-        () => {
-          document.getElementById("fk1").value = _getBit(fk1);
-        },
-        () => {
-          document.getElementById("SW1").value = _getBit(sw);
-        },
-        () => {
-          document.getElementById("fk2").value = _getBit(fk2);
-        },
-        () => {
-          document.getElementById("IPm1").value = _getBit(ipm1);
-        },
-        enc => {
-          document.getElementById("encrypt1").value = enc;
-        },
-        enc => {
-          document.getElementById("encrypt2").value = enc;
-        }
+        () => document.getElementById("IP1").value = _getBit(ip),
+        () => document.getElementById("fk1").value = _getBit(fk1),
+        () => document.getElementById("SW1").value = _getBit(sw),
+        () => document.getElementById("fk2").value = _getBit(fk2),
+        () => document.getElementById("IPm1").value = _getBit(ipm1),
+        enc => document.getElementById("encrypt1").value = enc,
+        enc => document.getElementById("encrypt2").value = enc
       ];
-      if (plaintext1[i].charCodeAt(0) > 255) {
-        const setBit = [plaintext1[i].charCodeAt(0) >> 8, plaintext1[i].charCodeAt(0) & 255];
-        for (let j of setBit.keys()) {
-          ip = _ip(setBit[j], 8, 1);
-          fk1 = _fk(ip, k1);
-          sw = _sw(fk1);
-          fk2 = _fk(sw, k2);
-          ipm1 = _ip(_convertTen(fk2), 8, 2);
-          encBit |= _convertTen(ipm1);
-          j == 0 ? encBit = encBit << 8 : encBit = encBit;
-          for (let k = 0; k < printEnc.length - 2; k++) {
-            printEnc[k]();
-            await _sleep(100);
-          }
-          enc = String.fromCharCode(encBit);
-          printEnc[5](encF + enc);
-        }
-        encF += enc;
-        printEnc[6](encF);
-        await _sleep(100);
-      } else {
-        ip = _ip(plaintext1[i].charCodeAt(0), 8, 1);
-        fk1 = _fk(ip, k1);
-        sw = _sw(fk1);
-        fk2 = _fk(sw, k2);
-        ipm1 = _ip(_convertTen(fk2), 8, 2);
-        encF += String.fromCharCode(_convertTen(ipm1));
-        for (let j = 0; j < printEnc.length - 2; j++) {
-          printEnc[j]();
-          await _sleep(100);
-        }
-        printEnc[5](encF);
-        await _sleep(100);
-        printEnc[6](encF);
-        await _sleep(100);
-      }
+if (plaintext1[i].charCodeAt(0) > 255) {
+  const setBit = [plaintext1[i].charCodeAt(0) >> 8, plaintext1[i].charCodeAt(0) & 255];
+  for (let j of setBit.keys()) {
+    ip = _ip(setBit[j], 8, 1);
+    fk1 = _fk(ip, k1);
+    sw = _sw(fk1);
+    fk2 = _fk(sw, k2);
+    ipm1 = _ip(_convertTen(fk2), 8, 2);
+    encBit |= _convertTen(ipm1);
+    j == 0 ? encBit = encBit << 8 : encBit = encBit;
+    for (let k = 0; k < printEnc.length - 2; k++) {
+      printEnc[k]();
+      await _sleep(100);
+    }
+    enc = String.fromCharCode(encBit);
+    printEnc[5](encF + enc);
+  }
+  encF += enc;
+  printEnc[6](encF);
+  await _sleep(100);
+} else {
+  ip = _ip(plaintext1[i].charCodeAt(0), 8, 1);
+  fk1 = _fk(ip, k1);
+  sw = _sw(fk1);
+  fk2 = _fk(sw, k2);
+  ipm1 = _ip(_convertTen(fk2), 8, 2);
+  encF += String.fromCharCode(_convertTen(ipm1));
+  for (let j = 0; j < printEnc.length - 2; j++) {
+    printEnc[j]();
+    await _sleep(100);
+  }
+  printEnc[5](encF);
+  await _sleep(100);
+  printEnc[6](encF);
+  await _sleep(100);
+}
     }
 
-    //복호화
+//복호화
 
-    for (let i = 0; i < encF.length; i++) {
-      let decBit = 0, ip, fk1, sw, fk2, ipm1;
-      const printDec = [
-        () => {
-          document.getElementById("IP2").value = _getBit(ip);
-        },
-        () => {
-          document.getElementById("fkOne").value = _getBit(fk1);
-        },
-        () => {
-          document.getElementById("SW2").value = _getBit(sw);
-        },
-        () => {
-          document.getElementById("fkTwo").value = _getBit(fk2);
-        },
-        () => {
-          document.getElementById("IPm2").value = _getBit(ipm1);
-        },
-        dec => {
-          document.getElementById("plaintext2").value = dec;
-        }
-      ]
-      if (encF[i].charCodeAt(0) > 255) {
-        const setBit = [encF[i].charCodeAt(0) >> 8, encF[i].charCodeAt(0) & 255];
-        for (let j of setBit.keys()) {
-          ip = _ip(setBit[j], 8, 1);
-          fk1 = _fk(ip, k2);
-          sw = _sw(fk1);
-          fk2 = _fk(sw, k1);
-          ipm1 = _ip(_convertTen(fk2), 8, 2);
-          decBit |= _convertTen(ipm1);
-          j == 0 ? decBit = decBit << 8 : decBit = decBit;
+for (let i = 0; i < encF.length; i++) {
+  let decBit = 0, ip, fk1, sw, fk2, ipm1;
+  const printDec = [
+    () => document.getElementById("IP2").value = _getBit(ip),
+    () => document.getElementById("fkOne").value = _getBit(fk1),
+    () => document.getElementById("SW2").value = _getBit(sw),
+    () => document.getElementById("fkTwo").value = _getBit(fk2),
+    () => document.getElementById("IPm2").value = _getBit(ipm1),
+    
+    dec => document.getElementById("plaintext2").value = dec
+  ]
+  if (encF[i].charCodeAt(0) > 255) {
+    const setBit = [encF[i].charCodeAt(0) >> 8, encF[i].charCodeAt(0) & 255];
+    for (let j of setBit.keys()) {
+      ip = _ip(setBit[j], 8, 1);
+      fk1 = _fk(ip, k2);
+      sw = _sw(fk1);
+      fk2 = _fk(sw, k1);
+      ipm1 = _ip(_convertTen(fk2), 8, 2);
+      decBit |= _convertTen(ipm1);
+      j == 0 ? decBit = decBit << 8 : decBit = decBit;
 
-          for (let [k, show] of printDec.entries()) {
-            if(k < printDec.length - 1) show();
-            await _sleep(100);
-          }
-          dec = String.fromCharCode(decBit);
-          printDec[5]() == "" ? printDec[5](dec) : printDec[5](decF + dec);
-          await _sleep(100);
-        }
-        decF += dec;
-      } else {
-        ip = _ip(encF[i].charCodeAt(0), 8, 1);
-        fk1 = _fk(ip, k2);
-        sw = _sw(fk1);
-        fk2 = _fk(sw, k1);
-        ipm1 = _ip(_convertTen(fk2), 8, 2);
-        decF += String.fromCharCode(_convertTen(ipm1));
-
-        for (let [j, show] of printDec.entries()) {
-          if(j < printDec.length -1) show();
-          await _sleep(100);
-        }
-        printDec[5](decF);
+      for (let [k, show] of printDec.entries()) {
+        if (k < printDec.length - 1) show();
         await _sleep(100);
       }
+      dec = String.fromCharCode(decBit);
+      printDec[5]() == "" ? printDec[5](dec) : printDec[5](decF + dec);
+      await _sleep(100);
     }
+    decF += dec;
+  } else {
+    ip = _ip(encF[i].charCodeAt(0), 8, 1);
+    fk1 = _fk(ip, k2);
+    sw = _sw(fk1);
+    fk2 = _fk(sw, k1);
+    ipm1 = _ip(_convertTen(fk2), 8, 2);
+    decF += String.fromCharCode(_convertTen(ipm1));
+
+    for (let [j, show] of printDec.entries()) {
+      if (j < printDec.length - 1) show();
+      await _sleep(100);
+    }
+    printDec[5](decF);
+    await _sleep(100);
+  }
+}
   }
 }
